6dc512f1fc1df7b4020111dc56379114b62ea847
Apurba <palapurba2004@gmail.com>
2025-01-09 10:03:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const canvas = document.getElementById("canvas");
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
+const loader = new GLTFLoader();
+
+loader.load(
+  "./mystical_forest_cartoon.glb",
+  function (glb) {
+    console.log(glb);
+    const model = glb.scene;
+    model.scale.set(0.01, 0.01, 0.01);
+    glb.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    scene.add(glb.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+const sun = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(sun);
+sun.position.set(100, 100, 100);
+sun.target.position.set(0, 0, 0);
+sun.shadow.camera.left = -50;
+sun.shadow.camera.right = 50;
+sun.shadow.camera.top = 50;
+sun.shadow.camera.bottom = -50;
+sun.shadow.mapSize.width = 2048; // Increase for sharper shadows
+sun.shadow.mapSize.height = 2048;
+
+const helper = new THREE.DirectionalLightHelper(sun, 10);
+scene.add(helper);
+
+const ShadowCamera = new THREE.CameraHelper(sun.shadow.camera);
+scene.add(ShadowCamera);
+console.log("shadw ", sun.shadow);
+
+const light = new THREE.AmbientLight(0x404040, 3); // soft white light
+scene.add(light);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+scene.add(camera);
+camera.position.set(-3, 3, 50);
+
+// Initialize OrbitControls
+const controls = new OrbitControls(camera, canvas);
+// Enable damping for smoother interaction
+controls.enableDamping = true;
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+function animate() {
+  // console.log(camera.position)
+
+  // Update controls
+  controls.update();
+
+  // Render the scene
+  renderer.render(scene, camera);
+}
+
+// Start the animation loop
+renderer.setAnimationLoop(animate);
+
+// shadows
+
+// 3:48
